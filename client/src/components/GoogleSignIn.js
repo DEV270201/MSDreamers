eedf7532913546c48a35775437a5a65170ddc89c
Priyal Babel <60316882+priyal-babel@users.noreply.github.com>
2021-09-23 00:16:02 +0530

    Google drive changes

--- client/src/components/GoogleSignIn.js
+++ client/src/components/GoogleSignIn.js
@@ -1,41 +1,60 @@
-import React from 'react';
-import {Button} from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { Button } from '@material-ui/core';
 import { GoogleLogin } from 'react-google-login';
 import GoogleIcon from '../Icons/GoogleIcon';
 import CLIENT_ID from '../config/conf';
 import Axios from 'axios';
 
 const GoogleSignIn = () => {
+  useEffect(() => {
+    console.log('Use effect is working!!!!!!!!!!!');
+    async function newFunc() {
+      const resp = await Axios.get(
+        'https://www.googleapis.com/drive/v3/files/1bBAQngEK9dX6_CX13WgFUVbbB2ov38cN'
+      );
+      console.log('Responseeeeeeeeeeeeee: ', resp);
+    }
+    newFunc();
+  });
 
   const googleSuccess = async (res) => {
     // console.log(res)
-    try{
-      const resp = await Axios.post('http://localhost:4000/users/googleSignIn',{token: res.tokenId});
-      
+    try {
+      const resp = await Axios.post(
+        'http://localhost:4000/users/googleSignIn',
+        { token: res.tokenId }
+      );
+      console.log(resp);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const googleFailure = (err) => {
     console.log(err);
-    console.log("Sign in unsuccessful!")
-  }
+    console.log('Sign in unsuccessful!');
+  };
 
-  return(
-
+  return (
     <GoogleLogin
       clientId={CLIENT_ID}
-      render={renderProps => (
-          <Button color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<GoogleIcon/>} variant="contained">
-            Google Sign In
-          </Button>
-          )}
+      render={(renderProps) => (
+        <Button
+          color='primary'
+          fullWidth
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+          startIcon={<GoogleIcon />}
+          variant='contained'
+        >
+          Google Sign In
+        </Button>
+      )}
       onSuccess={googleSuccess}
       onFailure={googleFailure}
-      cookiePolicy={"single_host_origin"}
-      />
-  )
-}
+      cookiePolicy={'single_host_origin'}
+    />
+  );
+};
 
 export default GoogleSignIn;
--- routes/GoogleDriveRouter.js
+++ routes/GoogleDriveRouter.js
@@ -1,59 +1,58 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const {google} = require('googleapis');
-const fs = require("fs");
-const credentials = require('../credentials.json')
+const { google } = require('googleapis');
+const fs = require('fs');
+const credentials = require('../credentials.json');
 
-const scopes = [
-  'https://www.googleapis.com/auth/drive',
-];
+const scopes = ['https://www.googleapis.com/auth/drive'];
 
 const oauth2Client = new google.auth.JWT(
   credentials.client_email,
   null,
   credentials.private_key,
-  scopes
+  scopes,
+  null
 );
 
+router.post('/', async (_req, res) => {
+  const folderId = '1wq1ZDJH2WQ6BK07uT3tusqEOXlaQ2I9n';
+  var fileMetadata = {
+    name: 'Dogs.jpg', // file name that will be saved in google drive
+    parents: [folderId],
+  };
 
+  var media = {
+    mimeType: 'image/jpg',
+    body: fs.createReadStream('utils/dogs.jpg'), // Reading the file from our server
+  };
 
-router.post('/', async(req,res) => {
+  // Authenticating drive API
+  const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
-      var fileMetadata = {
-        name: "Sunrise123", // file name that will be saved in google drive
-      };
-    
-      var media = {
-        mimeType: 'image/webp',
-        body: fs.createReadStream('utils/pic.webp'), // Reading the file from our server
-      };
-    
-      // Authenticating drive API
-      const drive = google.drive({ version: 'v3', auth: oauth2Client });
-    
-      // Uploading Single image to drive
-      drive.files.create(
-        {
-          resource: fileMetadata,
-          media: media,
-        },
-        async (err, file) => {
-          if (err) {
-            // Handle error
-            console.error("errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr" , err);
-    
-            return res
-              .status(400)
-              .json({ errors: [{ msg: 'Server Error try again later' }] });
-          } else {
-            // if file upload success then return the unique google drive id
-            res.status(200).json({
-              status : "success",
-              fileID: file.data.id,
-            });
-          }
-        }
-      );
+  // Uploading Single image to drive
+  await drive.files.create(
+    {
+      resource: fileMetadata,
+      media: media,
+    },
+    async (err, file) => {
+      if (err) {
+        // Handle error
+        console.error('errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', err);
+
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Server Error try again later' }] });
+      } else {
+        // if file upload success then return the unique google drive id
+        console.log(file);
+        res.status(200).json({
+          status: 'success',
+          fileID: file.data.id,
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;

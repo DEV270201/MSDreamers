856f3e795f3f2af99421eaf0ee39a88afda8bf87
DEV270201 <devansh.js@somaiya.edu>
2021-10-27 02:20:22 +0530

    modified navbar
    Co-authored-by: Priyal Babel <priyal-babel@users.noreply.github.com>

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,11 @@ import Navbar from './Navbar';
 
 const Landing = () => {
   return(
-     <Navbar/>
+    <div>
+    <Navbar/>
+    
+    <img src="https://images.unsplash.com/photo-1592819695396-064b9572a660?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" class="img-fluid" style={{width: "100%", height: "100%"}}></img>
+  </div>
   );
 };
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,35 +2,39 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-
     return(
         <header>
         <nav className="navbar navbar-expand-lg navbar-dark mynavbar">
-        <div className="container mycont">
-        <NavLink className="navText" to="/">
+        <div className="container-fluid mycont">
+          <div className="divider">
+        <NavLink to="/">
           <img src="https://www.avanse.com/blog/wp-content/uploads/2014/08/5.jpg" alt="" width="30" height="24"/>
-            <span style={{marginLeft: '30px'}}>MsDreamers</span>
         </NavLink>
+            <span style={{marginLeft:'30px'}}>MsDreamers</span>
+            </div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
       </div>
       <div className="container">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+          <ul className="navbar-nav ml-auto mb-2 mb-lg-0 myul">
+            <li className="nav-item nav_li_center">
               <NavLink to="/about" className="navText">About</NavLink>
             </li>
-            <li className="nav-item">
+            <li className="nav-item nav_li_center">
               <NavLink to="/forum" className="navText">Forum</NavLink>
             </li>
-            <li className="nav-item">
+            <li className="nav-item nav_li_center">
               <NavLink to="/resources" className="navText">Resources</NavLink>
             </li>
-            <span className="navText">|</span>
-            <li className="nav-item dropdown">
+            <li className="nav-item dropdown nav_li_center">
               <NavLink to="/login" className="navText">Login</NavLink>
-            </li>
-            <li className="nav-item dropdown">
+            <span className="divider">|</span>
               <NavLink to="/register" className="navText">Register</NavLink>
             </li>
+            {/* <li className="nav-item dropdown">
+            </li> */}
           </ul>
         </div>
       </div>

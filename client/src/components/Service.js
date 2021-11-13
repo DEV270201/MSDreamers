eb3c28c4cc942e3ee844ce096e5615503c1fe6f5
DEV270201 <devansh.js@somaiya.edu>
2021-11-14 01:14:47 +0530

    fixed animation bug
    Co-authored-by: Priyal Babel <priyal-babel@users.noreply.github.com>

--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -1,58 +1,64 @@
 import React,{useEffect} from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1000,
+})
 
 const Service = ({service,keys})=>{
 
-    useEffect(()=>{
-    let card = document.querySelector(".card"+String(keys));
-    console.log("useeffect");
+    // useEffect(()=>{
+    // let card = document.querySelector(".card"+String(keys));
+    // console.log("useeffect");
     
-      const checkInViewPort = ()=>{
-        console.log("key : " + keys + " top : " + card.getBoundingClientRect().top);
-        return card.getBoundingClientRect().top <= (window.innerHeight||document.documentElement.clientHeight);  //will return true if the element is present in the viewport or not
-      }
+    //   const checkInViewPort = ()=>{
+    //     console.log("key : " + keys + " top : " + card.getBoundingClientRect().top);
+    //     return card.getBoundingClientRect().top <= (window.innerHeight||document.documentElement.clientHeight);  //will return true if the element is present in the viewport or not
+    //   }
 
-      const animate = ()=>{
-        // console.log("hello");
-          if(checkInViewPort()){
-            console.log(keys);
-            displayCard();
-          } 
-          // else {
-          //   removeCard();
-          // }
-      }
+    //   const animate = ()=>{
+    //     // console.log("hello");
+    //       if(checkInViewPort()){
+    //         console.log(keys);
+    //         displayCard();
+    //       } 
+    //       else {
+    //         removeCard();
+    //       }
+    //   }
 
-      const displayCard = () => {
-        if (keys % 2 === 0){ // If odd then card will appear from left
-          card.classList.add('right_animate')  // Adding animation class to card
-        } else {
-          card.classList.add('left_animate')
-        }
-      }
+      // const displayCard = () => {
+      //   if (true){ // If odd then card will appear from left
+      //     card.classList.add('left_animate')  // Adding animation class to card
+      //   } else {
+      //     card.classList.add('left_animate')
+      //   }
+      // }
 
-      const removeCard = () => {
-        if (keys % 2 === 0){ // If odd then card will appear from left
-          card.classList.remove('right_animate')  // Adding animation class to card
-        } else {
-          card.classList.remove('left_animate')
-        }
-      }
+      // const removeCard = () => {
+      //   if (true){ // If odd then card will appear from left
+      //     card.classList.remove('left_animate')  // Adding animation class to card
+      //   } else {
+      //     card.classList.remove('left_animate')
+      //   }
+      // }
 
-      window.addEventListener("scroll",animate);
+  //     window.addEventListener("scroll",animate);
       
-      return(()=>{
-        console.log("removed");
-       window.removeEventListener("scroll",animate);
-      });
+  //     return(()=>{
+  //       console.log("removed");
+  //      window.removeEventListener("scroll",animate);
+  //     });
  
-   },[keys]);
+  //  },[keys]);
    
 return (
         <>
-<div className={"card mx-auto mb-3 mt-5 " + "card"+String(keys)} style={{maxWidth: "85%"}}>
+<div data-aos={keys % 2 === 0 ? "fade-right" :"fade-left" } className="card mx-auto mb-3 mt-5" style={{maxWidth: "85%"}}>
   <div className="row no-gutters">
     <div className={keys % 2 === 0 ? "col-md-4 order-md-2 order-sm-1" :"col-md-4 order-md-1 order-sm-1" }>
-      <img className="service_img" src={service[0]} alt={service[1]}></img>
+      <img className="service_img" src={service[0]} alt={service[1]} style={{height: '250px'}}></img>
     </div>
     <div className={keys % 2 === 0 ? "col-md-8 order-md-1 order-sm-2" :"col-md-8 order-md-2 order-sm-2" }>
       <div className="card-body">
@@ -62,7 +68,6 @@ return (
     </div>
   </div>
 </div>
-
         </>
     );
 }
---
+++ client/src/data/serviceData.js
@@ -0,0 +1,8 @@
+const arr = [
+    ["https://iteachu.uaf.edu/wp-content/uploads/sites/623/2016/05/Screen-Shot-2016-08-09-at-11.14.01-AM.png", "Forum", "Talk to other people!!"],
+    ["https://www.friesens.com/wp-content/uploads/2020/09/Best-Resources-to-Read-Books-Online-2000x1200-1.jpg", "Resources", "Free books!!"],
+    ["https://st2.depositphotos.com/1105977/5461/i/600/depositphotos_54615585-stock-photo-old-books-on-wooden-table.jpg", "Flashcards", "Learn new words!!"],
+    ["https://www.buzbeast.com/wp-content/uploads/2021/03/GRE-at-Home3.jpg", "Test papers", "Give online tests and practice your knowledge!!"]
+  ]
+
+export default arr;
